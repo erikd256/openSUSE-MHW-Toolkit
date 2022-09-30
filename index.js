a54227ec4c593ec932b2d0eb384fc56bfa49b28d
erikd256 <62072464+erikd256@users.noreply.github.com>
2022-09-30 19:17:49 +0300

    update spinner

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
+var Spinner = require('cli-spinner').Spinner;
 var arguments = require('yargs/yargs')(process.argv.slice(2)).argv;
-const ora = require('ora')
 var shell = require('shelljs');
 var wget = require('node-wget');
 var prompt = require('prompt-sync')({
@@ -46,36 +46,29 @@ if (arguments.init){
         };
     }
     var flavour = prompt("Flavour used for device (TumbleWeed-AARCH64 | TumbleWeed-ARMv7 | Leap-AARCH64 | Leap-ARMv7 | TumbleWeed-ARMv6) : ");
+    var spinner = new Spinner('Downloading rootfs tarball for '+flavour);
+    spinner.setSpinnerString('|/-\\');
+    spinner.start();
     switch (flavour) {
     case 'TumbleWeed-AARCH64':
-        const spinner = ora('Downloading rootfs tarball...').start();
-        spinner.color = 'green';
         wget({url: "https://download.opensuse.org/ports/aarch64/tumbleweed/appliances/openSUSE-Tumbleweed-ARM-JeOS.aarch64-rootfs.aarch64.tar.xz", dest: chrootDir+"/rootfs.tar.xz"});
         spinner.stop();
         break;
     case 'TumbleWeed-ARMv7':
-        const spinner2 = ora('Downloading rootfs tarball...').start();
-        spinner2.color = 'green';
         wget({url: "https://download.opensuse.org/ports/armv7hl/tumbleweed/appliances/openSUSE-Tumbleweed-ARM-JeOS.armv7-rootfs.armv7l.tar.xz", dest: chrootDir+"/rootfs.tar.xz"});
-        spinner2.stop();
+        spinner.stop();
         break;
     case 'Leap-AARCH64':
-        const spinner3 = ora('Downloading rootfs tarball...').start();
-        spinner3.color = 'green';
         wget({url: "https://download.opensuse.org/ports/aarch64/distribution/leap/15.4/appliances/openSUSE-Leap-15.4-ARM-JeOS.aarch64-rootfs.aarch64.tar.xz", dest: chrootDir+"/rootfs.tar.xz"});
-        spinner3.stop();
+        spinner.stop();
         break;
     case 'Leap-ARMv7':
-        const spinner4 = ora('Downloading rootfs tarball...').start();
-        spinner4.color = 'green';
         wget({url: "https://download.opensuse.org/ports/armv7hl/distribution/leap/15.4/appliances/openSUSE-Leap-15.4-ARM-JeOS.armv7-rootfs.armv7l.tar.xz", dest: chrootDir+"/rootfs.tar.xz"});
-        spinner4.stop();
+        spinner.stop();
         break;
     case 'TumbleWeed-ARMv6':
-        const spinner5 = ora('Downloading rootfs tarball...').start();
-        spinner5.color = 'green';
         wget({url: "https://download.opensuse.org/ports/armv6hl/tumbleweed/appliances/openSUSE-Tumbleweed-ARM-JeOS.armv6-rootfs.armv6l.tar.xz", dest: chrootDir+"/rootfs.tar.xz"});
-        spinner5.stop();
+        spinner.stop();
         break;
     default:
     console.log('Please put in a valid flavour:\n(TumbleWeed-AARCH64 | TumbleWeed-ARMv7 | Leap-AARCH64 | Leap-ARMv7 | TumbleWeed-ARMv6)');
